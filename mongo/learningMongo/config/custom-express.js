252a96d241248f6cb2ea1ea413c84da91f53d4a2
pongelupe <pongelupe1998@gmail.com>
2018-03-12 10:15:29 -0300

    nova estrutura do projeto

--- mongo/learningMongo/config/custom-express.js
+++ mongo/learningMongo/config/custom-express.js
@@ -1,18 +1,19 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var path = require('path');
 
-module.exports = function () {
+module.exports = function() {
     var app = express();
 
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
 
-    consign()
-        .include('controllers')
-        .then('persistence')
-        .then('services')
+    consign({cwd: 'app'})
+        .include('models')
+        .then('api')
+        .then('routes')
         .into(app);
 
     return app;
