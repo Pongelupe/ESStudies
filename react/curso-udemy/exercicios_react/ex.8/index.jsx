263f4a6865b126b65a1272bae61034067d128e0a
pongelupe <pongelupe1998@gmail.com>
2018-05-04 18:37:20 -0300

    continuacao do curos react

--- react/curso-udemy/exercicios_react/ex.8/index.jsx
+++ react/curso-udemy/exercicios_react/ex.8/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Field from './field'
+import ClassComponent from './classComponent'
 
 ReactDOM.render((
-    <Field initialValue={'React'} />
+    <ClassComponent initialValue={10} label={'ola React contador'} />
 )
     , document.getElementById('app'))
