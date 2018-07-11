bc9c7287af70e3a598ee413af834b6a279da0579
pongelupe <pongelupe1998@gmail.com>
2018-07-10 22:36:20 -0300

    novo curso de angular 6 alura

---
+++ angular2/alurapic2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Melao';
+  url = 'https://saudavelefeliz.com/wp-content/uploads/2015/01/melao.jpg'
+}
---
+++ angular2/alurapic2/src/app/photos/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent {
+
+  @Input() description;
+  @Input() url;
+
+}
