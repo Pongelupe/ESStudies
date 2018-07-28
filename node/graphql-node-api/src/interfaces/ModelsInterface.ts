f47c62cdfe38dde5b78e280219e1e46b5a30d292
pongelupe <pongelupe1998@gmail.com>
2018-07-28 09:40:36 -0300

    integracao com mysql pelo sequelize

---
+++ node/graphql-node-api/src/interfaces/ModelsInterface.ts
@@ -0,0 +1,11 @@
+import { UserModel } from "../models/UserModel";
+import { PostModel } from "../models/PostModel";
+import { CommentModel } from "../models/CommentModel";
+
+export interface ModelsInterface {
+
+    User: UserModel;
+    Post: PostModel;
+    Comment : CommentModel;
+
+}
