a1487fb73b0fd0ecb7272474cc1f9c5292c8823c
Pongelupe <pongelupe1998@gmail.com>
2018-07-30 21:21:45 -0300

    inicio do signup

--- angular2/alurapic2/src/app/app.routing.module.ts
+++ angular2/alurapic2/src/app/app.routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from '@angular/router';
-
-import { PhotoListComponent } from './photos/photo-list/photo-list.component';
-import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
-import { NotFoundComponent } from "./errors/not-found/not-found.component";
-import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
-import { SigninComponent } from './home/signin/signin.component';
+import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from "./core/auth/auth.guard";
+import { NotFoundComponent } from "./errors/not-found/not-found.component";
+import { SigninComponent } from './home/signin/signin.component';
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
+import { SignupComponent } from "./home/signup/signup.component";
+
 
 const routes: Routes = [
     { path: '', component: SigninComponent, canActivate: [AuthGuard] },
+    { path: 'signup', component: SignupComponent },
     { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver } },
     { path: 'p/add', component: PhotoFormComponent },
     { path: '**', component: NotFoundComponent }
--- angular2/alurapic2/src/app/home/home.module.ts
+++ angular2/alurapic2/src/app/home/home.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SigninComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     VmessageModule,
     RouterModule
   ],
-  declarations: [SigninComponent]
+  declarations: [SigninComponent, SignupComponent]
 })
 export class HomeModule { }
