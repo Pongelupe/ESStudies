da2d40511aeeca57acf9409a9b372497ac0e9709
Pongelupe <pongelupe1998@gmail.com>
2019-09-25 00:20:07 -0300

    first component

---
+++ react/react-complete-course-19/monsters-rolodex/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import { CardList } from './components/card-list/card-list.component';
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <CardList>
+          {
+            this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+          }
+        </CardList>
+      </div>
+    );
+  }
+
+}
+
+export default App;
---
+++ react/react-complete-course-19/monsters-rolodex/src/components/card-list/card-list.component.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import './card-list.styles.css'
+
+export const CardList = (props) => <div className='card-list'>{props.children}</div>
