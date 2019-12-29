c973da8566c0395a928b30a0c623e7d76b5bc7d9
Pongelupe <pongelupe1998@gmail.com>
2019-12-29 14:23:58 -0300

    more react

--- react/react-complete-course-19/monsters-rolodex/src/components/card-list/card-list.component.js
+++ react/react-complete-course-19/monsters-rolodex/src/components/card-list/card-list.component.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
 import './card-list.styles.css'
+import { Card } from '../card/card.component';
 
-export const CardList = (props) => <div className='card-list'>{props.children}</div>
+export const CardList = props =>
+    <div className='card-list'>
+        {
+            props.monsters.map(monster => <Card key={monster.id} monster={monster} />)
+        }
+    </div>
